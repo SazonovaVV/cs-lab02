7ce94d9b2650aaa4c6456314cd22e5688e0b4ed1
u211-11 <u211-11@public.mpei.local>
2021-04-05 16:09:41 +0300

    code:дбавлен вывод максимума

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-int a, b;
+int a, b, max;
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n';
 cout << "A - B = " << a - b << '\n';
 cout << "A * B = " << a * b << '\n';
 cout << "A / B = " << a / b << '\n';
+if(A>B) max=A; else max=B; cout<<max;
     return 0;
 }
